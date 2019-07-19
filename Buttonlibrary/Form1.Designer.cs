67ada0291da5a8716fcfcd88390567daf45f9154
zhenhua Liu <qiyubrother@outlook.com>
2019-07-19 17:37:58 +0800

    .

---
+++ Buttonlibrary/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Buttonlibrary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.roundButton1 = new Buttonlibrary.RoundButton();
+            this.roundButton2 = new Buttonlibrary.RoundButton();
+            this.roundButton3 = new Buttonlibrary.RoundButton();
+            this.SuspendLayout();
+            // 
+            // roundButton1
+            // 
+            this.roundButton1.EnterBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(100)))), ((int)(((byte)(120)))));
+            this.roundButton1.EnterForeColor = System.Drawing.Color.White;
+            this.roundButton1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.roundButton1.FlatAppearance.BorderSize = 0;
+            this.roundButton1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.roundButton1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.roundButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundButton1.HoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(119)))), ((int)(((byte)(169)))));
+            this.roundButton1.HoverForeColor = System.Drawing.Color.White;
+            this.roundButton1.Location = new System.Drawing.Point(40, 13);
+            this.roundButton1.Name = "roundButton1";
+            this.roundButton1.PressBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(100)))), ((int)(((byte)(120)))));
+            this.roundButton1.PressForeColor = System.Drawing.Color.White;
+            this.roundButton1.Radius = 18;
+            this.roundButton1.Size = new System.Drawing.Size(127, 42);
+            this.roundButton1.TabIndex = 0;
+            this.roundButton1.Text = "roundButton1";
+            this.roundButton1.UseVisualStyleBackColor = true;
+            // 
+            // roundButton2
+            // 
+            this.roundButton2.EnterBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(100)))), ((int)(((byte)(120)))));
+            this.roundButton2.EnterForeColor = System.Drawing.Color.White;
+            this.roundButton2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.roundButton2.FlatAppearance.BorderSize = 0;
+            this.roundButton2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.roundButton2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.roundButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundButton2.HoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(119)))), ((int)(((byte)(169)))));
+            this.roundButton2.HoverForeColor = System.Drawing.Color.White;
+            this.roundButton2.Location = new System.Drawing.Point(40, 72);
+            this.roundButton2.Name = "roundButton2";
+            this.roundButton2.PressBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(100)))), ((int)(((byte)(120)))));
+            this.roundButton2.PressForeColor = System.Drawing.Color.White;
+            this.roundButton2.Radius = 40;
+            this.roundButton2.Size = new System.Drawing.Size(127, 42);
+            this.roundButton2.TabIndex = 1;
+            this.roundButton2.Text = "roundButton2";
+            this.roundButton2.UseVisualStyleBackColor = true;
+            // 
+            // roundButton3
+            // 
+            this.roundButton3.EnterBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(100)))), ((int)(((byte)(120)))));
+            this.roundButton3.EnterForeColor = System.Drawing.Color.White;
+            this.roundButton3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.roundButton3.FlatAppearance.BorderSize = 0;
+            this.roundButton3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.roundButton3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.roundButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundButton3.HoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(119)))), ((int)(((byte)(169)))));
+            this.roundButton3.HoverForeColor = System.Drawing.Color.White;
+            this.roundButton3.Location = new System.Drawing.Point(239, 13);
+            this.roundButton3.Name = "roundButton3";
+            this.roundButton3.PressBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(100)))), ((int)(((byte)(120)))));
+            this.roundButton3.PressForeColor = System.Drawing.Color.White;
+            this.roundButton3.Radius = 30;
+            this.roundButton3.Size = new System.Drawing.Size(100, 100);
+            this.roundButton3.TabIndex = 2;
+            this.roundButton3.Text = "roundButton3";
+            this.roundButton3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 685);
+            this.Controls.Add(this.roundButton3);
+            this.Controls.Add(this.roundButton2);
+            this.Controls.Add(this.roundButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private RoundButton roundButton1;
+        private RoundButton roundButton2;
+        private RoundButton roundButton3;
+    }
+}
+
